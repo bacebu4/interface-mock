854cc2fd4af01dbbebe5bf5828e91dc1fbe4b3d9
bacebu4 <vasua14735@icloud.com>
2024-11-30 20:35:39 +0300

    feat: export type for interface mock

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
-export declare function createInterfaceMock<T extends object>(
-  impl: T,
-): T & { verify: (cb: (mock: T) => unknown, props?: { times?: number }) => void };
+export type InterfaceMock<T> = T & {
+  verify: (cb: (mock: T) => unknown, props?: { times?: number }) => void;
+};
+
+export declare function createInterfaceMock<T extends object>(impl: T): InterfaceMock<T>;
